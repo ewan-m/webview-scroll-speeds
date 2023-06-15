fec4b9292de5a033db8501ec69f27b5ca4bb14c7
ewan-m <ewanmorrrison@gmail.com>
2023-06-15 12:18:20 +0100

    Initial Commit

---
+++ Test/TestApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestApp.swift
+//  Test
+//
+//  Created by Ewan Morrison on 15/06/2023.
+//
+
+import SwiftUI
+
+@main
+struct TestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
