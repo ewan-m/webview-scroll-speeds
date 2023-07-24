e19ce697b0f8f9de43997f1b2c92918db0e5e2b5
ewan-m <ewanmorrrison@gmail.com>
2023-07-24 23:21:06 +0100

    commit2

--- Test/TestApp.swift
+++ Test/TestApp.swift
@@ -1,11 +1,31 @@
-//
-//  TestApp.swift
-//  Test
-//
-//  Created by Ewan Morrison on 15/06/2023.
-//
-
 import SwiftUI
+import WebKit
+
+struct ContentView: View {
+    var body: some View {
+        WebView(url: "https://expo.dev/")
+            .edgesIgnoringSafeArea(.all)
+    }
+}
+
+struct WebView: UIViewRepresentable {
+    let url: String
+    
+    func makeUIView(context: Context) -> WKWebView {
+        let webView = WKWebView()
+        if let url = URL(string: url) {
+            let request = URLRequest(url: url)
+            webView.load(request)
+            
+            print(UIScrollView.DecelerationRate.fast)
+            print(UIScrollView.DecelerationRate.normal)
+            print(webView.scrollView.decelerationRate)
+        }
+        return webView
+    }
+    
+    func updateUIView(_ uiView: WKWebView, context: Context) {}
+}
 
 @main
 struct TestApp: App {
